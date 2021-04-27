d91b0cfcdfd0b9ed22159466c38e5f969ac6073d
edonasi <hj-g20030@sist.ac.jp>
2021-04-27 12:31:59 +0900

    Windowの表示設定

--- Main.cpp
+++ Main.cpp
@@ -1,18 +1,53 @@
-#include "DxLib.h"
+//ヘッダファイルの読み込み
+#include "DxLib.h";//必ず必要
+
+//マクロ定義
+#define GAME_TITLE "ゲームタイトル"	//ゲームタイトル
+#define GAME_WIDTH 1280				//ゲームの横幅
+#define GAME_HEIGHT 720				//ゲームの縦幅
+#define GAME_COLOR 32				//ゲームの解像度
+#define BACK_GROUND_COLOR 255,255,255	//ゲームの背景色
+#define GAME_ICON_ID 333	//ゲームのICONのID
 
 // プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
+	//ウィンドウモードに設定
+	ChangeWindowMode(TRUE);
+	//ゲームタイトルの設定
+	SetMainWindowText(GAME_TITLE);
+	//ゲームウィンドウ状態設定
+	SetGraphMode(GAME_WIDTH, GAME_HEIGHT, GAME_COLOR);
+	//背景色の設定
+	SetBackgroundColor(BACK_GROUND_COLOR);
+	//ゲームのICONを設定
+	SetWindowIconID(GAME_ICON_ID);
+
 	if (DxLib_Init() == -1)		// ＤＸライブラリ初期化処理
 	{
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
-	DrawPixel(320, 240, GetColor(255, 255, 255));	// 点を打つ
+	while (1)
+	{
+		//キーが押されたとき
+		if (CheckHitKeyAll() != 0)
+		{
+			//無限ループを抜ける
+			break;
+		}
 
-	WaitKey();				// キー入力待ち
+		//メッセージを受け取り続ける、×などでウィンドウが閉じたとき
+		if (ProcessMessage() != 0) 
+		{
+			//無限ループを抜ける
+			break;
+		}
+	}
 
-	DxLib_End();				// ＤＸライブラリ使用の終了処理
+	// ＤＸライブラリ使用の終了処理準備(return 0でソフトが終了する)
+	DxLib_End();				
 
-	return 0;				// ソフトの終了 
+	// ソフトの終了
+	return 0;				 
 }
